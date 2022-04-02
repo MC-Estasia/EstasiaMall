880373e8c3777b204456f722d3196feace9f0de7
marc.liebe <git@marc-liebe.de>
2022-04-02 21:57:33 +0200

    🎉 Initial Commit

---
+++ src/main/java/de/mcestesia/model/ShopModel.java
@@ -0,0 +1,10 @@
+package de.mcestesia.model;
+
+import java.util.UUID;
+
+public class ShopModel {
+
+    private UUID ownerUUID;
+
+
+}
---
+++ src/main/java/de/mcestesia/util/Constants.java
@@ -0,0 +1,7 @@
+package de.mcestesia.util;
+
+public class Constants {
+
+    public static final String PREFIX = "§8[§5Mall§8] §7";
+
+}
